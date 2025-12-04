48996d48fe3b99d4810d08613235d04ad04052e9
ThilinaNirmalBandara <thilinab2002@gmail.com>
2025-12-04 22:57:39 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -110,7 +110,7 @@ export default function App() {
     setLoginError("");
 
     try {
-      const res = await fetch(`${BACKEND_URL}/api/login`, {
+      const res = await fetch(`${BACKEND_URL}/api/admin/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
